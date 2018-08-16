67ddaa53e0b7a28e697291e9d309b62412dab440
samarth12 <samarth12@live.com>
2018-08-16 15:38:01 -0700

    Updated

--- play.py
+++ play.py
@@ -8,6 +8,14 @@ idx =0
 count = 0
 truth = True
 
+def filtercontours(contours):
+    playercontours = list()
+    for c in contours:
+        rect = cv2.boundingRect(c)
+        # if contour is too big or too small its not player
+        if (rect[2] < 7 or rect[3] < 20) or (rect[2] > 60 or rect[3] > 100): continue
+        playercontours.append(c)
+    return playercontours
 
 while True:
 
@@ -50,57 +58,52 @@ while True:
 	prev = 0
 	font = cv2.FONT_HERSHEY_SIMPLEX
 
+
+	contours = filtercontours(contours)
 	for c in contours:
 		x,y,w,h = cv2.boundingRect(c)
+		player_img = img[y:y+h,x:x+w]
+		player_hsv = cv2.cvtColor(player_img,cv2.COLOR_BGR2HSV)
+
+		mask1 = cv2.inRange(player_hsv, lower_blue, upper_blue)
+		player1_result = cv2.bitwise_and(player_img, player_img, mask=mask1)
+		player1_result = cv2.cvtColor(player1_result,cv2.COLOR_HSV2BGR)
+		player1_result = cv2.cvtColor(player1_result,cv2.COLOR_BGR2GRAY)
+		count_chelsea = cv2.countNonZero(player1_result)
+
+		mask2 = cv2.inRange(player_hsv, lower_red, upper_red)
+		player2_result = cv2.bitwise_and(player_img, player_img, mask=mask2)
+		player2_result = cv2.cvtColor(player2_result,cv2.COLOR_HSV2BGR)
+		player2_result = cv2.cvtColor(player2_result,cv2.COLOR_BGR2GRAY)
+		count_liverpool = cv2.countNonZero(player2_result)
+
+		if(count_chelsea >= 5):
+
+			cv2.putText(img, 'Chelsea', (x-2, y-2), font, 0.8, (255,0,0), 2, cv2.LINE_AA)
+			cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
+
+		if(count_liverpool>=5):
+
+			cv2.putText(img, 'Liverpool', (x-2, y-2), font, 0.8, (0,0,255), 2, cv2.LINE_AA)
+			cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+	"""
+if((h>=1 and w>=1) and (h<=30 and w<=30)):
+	ball_img = img[y:y+h,x:x+w]
+
+	ball_hsv = cv2.cvtColor(ball_img,cv2.COLOR_BGR2HSV)
+
+	mask1 = cv2.inRange(ball_hsv, lower_yellow, upper_yellow)
+	ball_result = cv2.bitwise_and(ball_img, ball_img, mask=mask1)
+	ball_result = cv2.cvtColor(ball_result,cv2.COLOR_HSV2BGR)
+	ball_result = cv2.cvtColor(ball_result,cv2.COLOR_BGR2GRAY)
+	count_ball = cv2.countNonZero(ball_result)
 
 
-		if(h>=(1.5)*w):
-			if(w>10 and h>= 10):
-				idx = idx+1
-				player_img = img[y:y+h,x:x+w]
-				player_hsv = cv2.cvtColor(player_img,cv2.COLOR_BGR2HSV)
+	if(count_ball >= 10):
+		cv2.putText(img, 'football', (x-2, y-2), font, 0.8, (0,255,0), 2, cv2.LINE_AA)
+		cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
 
-				mask1 = cv2.inRange(player_hsv, lower_blue, upper_blue)
-				player1_result = cv2.bitwise_and(player_img, player_img, mask=mask1)
-				player1_result = cv2.cvtColor(player1_result,cv2.COLOR_HSV2BGR)
-				player1_result = cv2.cvtColor(player1_result,cv2.COLOR_BGR2GRAY)
-				count_chelsea = cv2.countNonZero(player1_result)
-				
-				mask2 = cv2.inRange(player_hsv, lower_red, upper_red)
-				player2_result = cv2.bitwise_and(player_img, player_img, mask=mask2)
-				player2_result = cv2.cvtColor(player2_result,cv2.COLOR_HSV2BGR)
-				player2_result = cv2.cvtColor(player2_result,cv2.COLOR_BGR2GRAY)
-				count_liverpool = cv2.countNonZero(player2_result)
-
-				if(count_chelsea >= 5):
-
-					cv2.putText(img, 'Chelsea', (x-2, y-2), font, 0.8, (255,0,0), 2, cv2.LINE_AA)
-					cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
-				else:
-					pass
-				if(count_liverpool>=5):
-
-					cv2.putText(img, 'Liverpool', (x-2, y-2), font, 0.8, (0,0,255), 2, cv2.LINE_AA)
-					cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
-				else:
-					pass
-		if((h>=1 and w>=1) and (h<=30 and w<=30)):
-			ball_img = img[y:y+h,x:x+w]
-
-			ball_hsv = cv2.cvtColor(ball_img,cv2.COLOR_BGR2HSV)
-
-			mask1 = cv2.inRange(ball_hsv, lower_yellow, upper_yellow)
-			ball_result = cv2.bitwise_and(ball_img, ball_img, mask=mask1)
-			ball_result = cv2.cvtColor(ball_result,cv2.COLOR_HSV2BGR)
-			ball_result = cv2.cvtColor(ball_result,cv2.COLOR_BGR2GRAY)
-			count_ball = cv2.countNonZero(ball_result)
-
-
-			if(count_ball >= 10):
-				cv2.putText(img, 'football', (x-2, y-2), font, 0.8, (0,255,0), 2, cv2.LINE_AA)
-				cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
-
-
+		"""
 	cv2.imwrite("./Cropped/frame%d.jpg" % count, result)
 	print 'Read a new frame: ', truth
 	count += 1
