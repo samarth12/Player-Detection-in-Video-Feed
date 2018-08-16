4c6b41fb424063630408b2d84c2a8b4b333cca35
samarth12 <samarth12@live.com>
2018-08-16 15:38:18 -0700

    Fixed typo

--- values.py
+++ values.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 
-color = np.uint8([[[0, 255, 255]]])
-hsvColor = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
+color = np.uint8([[[0, 0, 0]]])
+hsvColor = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
 print(hsvColor)
 lowerLimit = (hsvColor[0][0][0]-10,100,100)
 upperLimit = (hsvColor[0][0][0]+10,255,255)
